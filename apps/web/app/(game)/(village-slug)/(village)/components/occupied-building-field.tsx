96401e73bf9eba46a4def48fc7634cf2114b477e
Saku-Matti Syrjä <smsyrja@gmail.com>
2026-03-08 04:11:16 +0000

    feat: add village background + move level badge to bottom-right
    - Generated meadow background (mobile 9:16 + desktop 16:9)
    - Applied to village view via layout SCSS
    - Moved building level indicator from center to bottom-right corner
    - Level number no longer obscures building graphics

--- apps/web/app/(game)/(village-slug)/(village)/components/occupied-building-field.tsx
+++ apps/web/app/(game)/(village-slug)/(village)/components/occupied-building-field.tsx
@@ -207,7 +207,7 @@ const OccupiedBuildingFieldContent = ({
       )}
       {...props}
     >
-      <div className="absolute absolute-centering">
+      <div className="absolute bottom-0 right-0 z-10">
         <BuildingUpgradeIndicator
           isHovered={isHovered}
           buildingField={buildingField}
